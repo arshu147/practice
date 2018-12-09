071b5fc46d767f201727cb97423661231ef84d94
arshuts <arshu253@gmail.com>
2018-12-09 16:20:18 +0530

    calc

--- Git/src/newww/test.java
+++ Git/src/newww/test.java
@@ -2,5 +2,7 @@ package newww;
 
 public class test {
 public static void main(String[] args) {
+System.out.println("");
+
 System.out.println("");}
 }
---
+++ Git/src/newww/testt.java
@@ -0,0 +1,10 @@
+package newww;
+
+public class testt {
+	public static void main(String[] args) {
+		System.out.println("");
+		System.out.println("king");
+		System.out.println("");
+	}
+
+}
